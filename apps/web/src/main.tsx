55ebfa81076c904d320c5e48ba93e6c2f095bb15
Sergey Strebulaev <strebulaev@gmail.com>
2026-09-05 11:39:23 +0000

    fix: Add Vite types, fix App import

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App';
+import { App } from './App';
 import './styles/index.css';
 
 const queryClient = new QueryClient({
