a4d7bf6d8012d67f2217f5881e06908f1d6fa449
Sergey Strebulaev <strebulaev@gmail.com>
2026-09-08 06:51:13 +0000

    fix: move eslint disable to prisma type line

--- apps/api/src/services/prisma.ts
+++ apps/api/src/services/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
 const globalForPrisma = globalThis as unknown as {
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
   prisma: PrismaClient | undefined;
 };
 
